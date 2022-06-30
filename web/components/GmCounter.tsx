45ed97793434cfa252286b45df412d24f5ea03d2
Eric Zakariasson <eric.zakariasson@gmail.com>
2022-06-30 17:16:55 +0200

    feat: add gm counter component

---
+++ web/components/GmCounter.tsx
@@ -0,0 +1,29 @@
+import { BigNumber } from "ethers";
+import { FC } from "react";
+import { useContractRead } from "wagmi";
+import LFG from "../LFG.json";
+
+export const GmCounter: FC = () => {
+  const { data, isLoading, isError } = useContractRead({
+    addressOrName: LFG.address,
+    contractInterface: LFG.abi,
+    functionName: "gmCount",
+  });
+
+  if (isError) {
+    return <h1>Error 😔</h1>;
+  }
+
+  if (isLoading) {
+    return <h1>Loading...</h1>;
+  }
+
+  const count = BigNumber.isBigNumber(data) ? data.toNumber() : 0;
+
+  return (
+    <h1>
+      {count} gm{count === 1 ? "" : "'s"} so far ☕️{" "}
+      <span>{count === 0 && "(ngmi)"}</span>
+    </h1>
+  );
+};
