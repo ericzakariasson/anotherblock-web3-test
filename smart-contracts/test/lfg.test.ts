6b6c9b0ff7bf875db052a01c638fdf97023a34bb
Eric Zakariasson <eric.zakariasson@gmail.com>
2022-06-30 15:32:00 +0200

    test: add contract unit tests

--- smart-contracts/test/lfg.test.ts
+++ smart-contracts/test/lfg.test.ts
@@ -1,20 +1,30 @@
-import { expect, use } from 'chai';
-import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
-import { LFG } from '../typechain';
-import { ethers } from 'hardhat';
+import { expect, use } from "chai";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import { solidity } from "ethereum-waffle";
 
-describe('Drop V1 Unit Tests', () => {
+import { LFG } from "../typechain";
+import { ethers } from "hardhat";
+
+use(solidity);
+
+describe("Drop V1 Unit Tests", () => {
   let lfg: LFG;
   let owner: SignerWithAddress;
 
   beforeEach(async () => {
     [owner] = await ethers.getSigners();
-    const LFG = await ethers.getContractFactory('LFG');
+    const LFG = await ethers.getContractFactory("LFG");
     lfg = (await LFG.deploy()) as LFG;
     await lfg.deployed();
   });
 
-  it('should increase gm', async () => {});
+  it("should increase gm", async () => {
+    expect((await lfg.gmCount()).toNumber()).to.eq(0);
+    await lfg.gm();
+    expect((await lfg.gmCount()).toNumber()).to.eq(1);
+  });
 
-  it('should emit event', async () => {});
+  it("should emit event", async () => {
+    expect(await lfg.gm()).to.emit(lfg, "Wagmi");
+  });
 });
