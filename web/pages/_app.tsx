f62c07a3aaeb7295ad22809dfc5219145bfccc7b
Eric Zakariasson <eric.zakariasson@gmail.com>
2022-06-30 17:16:19 +0200

    feat: add wagmi connector

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -1,8 +1,33 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+
+import { WagmiConfig, createClient, configureChains, chain } from "wagmi";
+import { InjectedConnector } from "wagmi/connectors/injected";
+import { MetaMaskConnector } from "wagmi/connectors/metaMask";
+
+import { publicProvider } from "wagmi/providers/public";
+
+const { chains, provider, webSocketProvider } = configureChains(
+  [chain.rinkeby],
+  [publicProvider()]
+);
+
+const client = createClient({
+  autoConnect: true,
+  provider,
+  webSocketProvider,
+  connectors: [
+    new MetaMaskConnector({ chains }),
+    new InjectedConnector({ chains }),
+  ],
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <WagmiConfig client={client}>
+      <Component {...pageProps} />;
+    </WagmiConfig>
+  );
 }
 
-export default MyApp
+export default MyApp;
