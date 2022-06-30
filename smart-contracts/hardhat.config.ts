17b8a25328e7b879f290232a3d2912d40f10a385
Eric Zakariasson <eric.zakariasson@gmail.com>
2022-06-30 16:33:50 +0200

    feat: add rinkeby deployment

--- smart-contracts/hardhat.config.ts
+++ smart-contracts/hardhat.config.ts
@@ -1,18 +1,18 @@
-import '@nomiclabs/hardhat-ethers';
-import 'hardhat-deploy-ethers';
-import '@nomiclabs/hardhat-etherscan';
-import '@typechain/hardhat';
-import 'hardhat-deploy';
-import 'hardhat-abi-exporter';
+import "@nomiclabs/hardhat-ethers";
+import "hardhat-deploy-ethers";
+import "@nomiclabs/hardhat-etherscan";
+import "@typechain/hardhat";
+import "hardhat-deploy";
+import "hardhat-abi-exporter";
 
-import * as dotenv from 'dotenv';
-import { HardhatUserConfig, task } from 'hardhat/config';
+import * as dotenv from "dotenv";
+import { HardhatUserConfig, task } from "hardhat/config";
 
 dotenv.config();
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
-task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
 
   for (const account of accounts) {
@@ -24,16 +24,16 @@ const INFURA_KEY = process.env.INFURA_ID;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
 const config: HardhatUserConfig = {
-  solidity: '0.8.4',
-  defaultNetwork: 'hardhat',
+  solidity: "0.8.4",
+  defaultNetwork: "hardhat",
 
   networks: {
-    // rinkeby: {
-    //   url: `https://rinkeby.infura.io/v3/${INFURA_KEY}`,
-    //   accounts: [`${PRIVATE_KEY}`],
-    // },
+    rinkeby: {
+      url: `https://rinkeby.infura.io/v3/${INFURA_KEY}`,
+      accounts: [`${PRIVATE_KEY}`],
+    },
     localhost: {
-      url: 'http://127.0.0.1:8545',
+      url: "http://127.0.0.1:8545",
       saveDeployments: true,
     },
   },
@@ -43,20 +43,20 @@ const config: HardhatUserConfig = {
     },
   },
   paths: {
-    deployments: './deployments',
+    deployments: "./deployments",
   },
   etherscan: {
     apiKey: process.env.ETHERSCAN_API_KEY as string,
   },
   typechain: {
-    outDir: 'typechain',
+    outDir: "typechain",
 
-    target: 'ethers-v5',
+    target: "ethers-v5",
     alwaysGenerateOverloads: true,
   },
 
   abiExporter: {
-    path: './abi',
+    path: "./abi",
     clear: true,
     flat: true,
     spacing: 2,
