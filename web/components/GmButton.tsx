311e6a339375d9bd0abc392dcc610fa91afb2e82
Eric Zakariasson <eric.zakariasson@gmail.com>
2022-07-01 13:36:07 +0200

    feat: update button hover style

--- web/components/GmButton.tsx
+++ web/components/GmButton.tsx
@@ -19,7 +19,7 @@ export const GmButton: FC<GmButtonProps> = ({ onGm }) => {
       <button
         disabled={isLoading}
         onClick={() => write()}
-        className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-lg px-5 py-2.5 text-center mt-6"
+        className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:scale-105 transition focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-lg px-5 py-2.5 text-center mt-6"
       >
         {isLoading ? "sending..." : "send gm 👋"}
       </button>
--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -58,7 +58,7 @@ const Home: NextPage = () => {
           </>
         ) : (
           <button
-            className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-lg px-5 py-2.5 text-center mt-6"
+            className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:scale-105 transition focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-lg px-5 py-2.5 text-center mt-6"
             onClick={() => connect()}
           >
             Connect Wallet
